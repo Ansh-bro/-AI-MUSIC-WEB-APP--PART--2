1bce6160f3ec02405956db1a75edbc4acf3649bf
Ansh-bro <87030975+Ansh-bro@users.noreply.github.com>
2022-04-11 18:59:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+song = "";
+function setup(){
+    canvas = createCanvas(700,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,700,500);
+}
+
+function preload(){
+    song = loadSound("music.mp3");
+}
